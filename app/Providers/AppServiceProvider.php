5da5f1162916c8e0e8db614b387dd85758e0fa04
kelly <k.vargas.c31@hotmail.com>
2018-11-07 00:58:41 -0500

    Error correction SQLSTATE[42000] width mariaBD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Error correction SQLSTATE[42000] width mariaBD
+        Schema::defaultStringLength(191);
     }
 
     /**
