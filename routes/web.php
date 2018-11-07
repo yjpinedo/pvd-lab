861acb3175296d4c988e3d8f0beda4249417b6f7
kelly <k.vargas.c31@hotmail.com>
2018-11-07 00:05:44 -0500

    Auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(
+    [
+        'verify' => true
+    ]
+);
+
+Route::get('/home', 'HomeController@index')->name('home');
