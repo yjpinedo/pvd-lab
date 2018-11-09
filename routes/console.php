8317b2a46ac2416ebe86b519049f4337839e03de
kelly <k.vargas.c31@hotmail.com>
2018-11-08 23:45:21 -0500

    Delete unnecessary routes

--- routes/console.php
+++ routes/console.php
@@ -13,4 +13,6 @@ use Illuminate\Foundation\Inspiring;
 |
 */
 
-//
+/*Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');*/
